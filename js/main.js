295b2d1eaa1bd22fdfe5aac07e55a2da14ee27e1
ula jeziorna <jeziornaula@wp.pl>
2021-05-26 19:37:48 +0200

    suma-zmiennych

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let firstNumber = 13;
+
+let secondNumber = 20;
+
+function add(num1, num2) {
+
+    let result = num1 + num2; 
+    return result;
+}
+
+console.log(add(firstNumber, secondNumber));
+
+/* To nie do zadania , ale chcę zobaczyć czy to ogarniam :) */
+
+let dodawanie = add(15, 15);
+
+console.log(dodawanie);
+
+
+
+
+
